0a1b2f715cd45f035b1e9834dd02091e609b4299
YNKI <88197049+ynki0@users.noreply.github.com>
2025-12-19 00:37:17 +0300

    Add files via upload

---
+++ _next/static/ZBMLgoyGSFJQ78eZ2drj0/_buildManifest.js
@@ -0,0 +1,31 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/36f3479c2c2e289d.js"
+  ],
+  "/_error": [
+    "static/chunks/1ad2675e5c1ee601.js"
+  ],
+  "/en": [
+    "static/chunks/b18f60b89037b2e6.js"
+  ],
+  "/ru": [
+    "static/chunks/8395b74c58791230.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/.//_next/:path+",
+        "destination": "/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/en",
+    "/ru"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
