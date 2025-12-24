23b7fd0e276d57dcc927615c5aadba68de74bdd9
YNKI <88197049+ynki0@users.noreply.github.com>
2025-12-25 00:00:17 +0300

    Add files via upload

---
+++ _next/static/PeyD3CMbvwVXKRhdJ5fhV/_buildManifest.js
@@ -0,0 +1,84 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/71ce2312a5f90c01.js"
+  ],
+  "/_error": [
+    "static/chunks/c21b35075ce16e04.js"
+  ],
+  "/about": [
+    "static/chunks/a4ee65f20a7f348e.js"
+  ],
+  "/app-development": [
+    "static/chunks/3b879899b20e615f.js"
+  ],
+  "/automation-ai-solutions": [
+    "static/chunks/e04a6031eca2bb1c.js"
+  ],
+  "/en": [
+    "static/chunks/bba985d8c02b1d8f.js"
+  ],
+  "/en/about": [
+    "static/chunks/e73bdec6ccc222c4.js"
+  ],
+  "/en/pricing": [
+    "static/chunks/0608f9f751f4d58e.js"
+  ],
+  "/pricing": [
+    "static/chunks/1c6c3340bab030fc.js"
+  ],
+  "/ru": [
+    "static/chunks/073a46b5914473a0.js"
+  ],
+  "/ru/about": [
+    "static/chunks/dd898b4e6865f28a.js"
+  ],
+  "/ru/pricing": [
+    "static/chunks/b3ac6ee58fc63d24.js"
+  ],
+  "/software-development": [
+    "static/chunks/7736209eaddde2c1.js"
+  ],
+  "/ui-ux-design": [
+    "static/chunks/a8abd6caf9542608.js"
+  ],
+  "/web-development": [
+    "static/chunks/c7b64fe3798dc670.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [
+      {
+        "source": "/:lang(en|ru)/:path*",
+        "destination": "/:path*"
+      },
+      {
+        "source": "/:lang(en|ru)",
+        "destination": "/"
+      }
+    ],
+    "beforeFiles": [
+      {
+        "source": "/.//_next/:path+",
+        "destination": "/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/about",
+    "/app-development",
+    "/automation-ai-solutions",
+    "/en",
+    "/en/about",
+    "/en/pricing",
+    "/pricing",
+    "/ru",
+    "/ru/about",
+    "/ru/pricing",
+    "/software-development",
+    "/ui-ux-design",
+    "/web-development"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/b3ac6ee58fc63d24.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/ru/pricing", [
+  "static/chunks/bca6b401d00385f1.js",
+  "static/chunks/8f05e98a0cf68d6d.js",
+  "static/chunks/ea3e0875166e57be.js",
+  "static/chunks/b7a5791f47e3c794.js",
+  "static/chunks/49c1f5c60fbf2ced.css",
+  "static/chunks/turbopack-6598163447ef7d48.js"
+])
